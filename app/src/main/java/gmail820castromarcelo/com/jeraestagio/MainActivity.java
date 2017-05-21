d063ee157fc8a8f56920bf2bd5c1f66a4459d753
Marcelo Garrido de Castro <marcelocastro820@gmail.com>
2017-05-21 17:12:00 -0400

    Tela de livros cadastrados finalizada

--- app/src/main/java/gmail820castromarcelo/com/jeraestagio/MainActivity.java
+++ app/src/main/java/gmail820castromarcelo/com/jeraestagio/MainActivity.java
@@ -1,13 +1,32 @@
 package gmail820castromarcelo.com.jeraestagio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected Button adicionar;
+    protected TextView livrosC;
+    protected ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adicionar = (Button) findViewById(R.id.adicionar);
+        livrosC = (TextView) findViewById(R.id.livrosC);
+        lista = (ListView) findViewById(R.id.lista);
+
+        adicionar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, CadastrarLivros.class);
+        startActivity(intent);
     }
 }
